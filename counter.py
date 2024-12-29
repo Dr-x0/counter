353fecad1217de9f33cadbaab695d7247d6bee9a
omar <oh439177@gmail.com>
2024-12-29 08:31:53 +0200

    Update counter.py

--- counter.py
+++ counter.py
@@ -2,45 +2,29 @@ from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
+import time
 
-root=Tk()
-root.geometry("300x200")
+root = Tk()
+root.geometry("300x250")
 root.title("counter")
-h=0
+h = 0
+
 def loop():
     global h
-    h+=1
-    Label(root,text=h,bg="#F5E216").place(x=120,y=5) 
+    h += 1
+    Label(root, text=h, bg="#F5E216").place(x=120, y=5) 
 
+def reset():
+    global h
+    h = 0
+    Label(root, text=h, bg="#F5E216").place(x=120, y=5)
 
-     
-   
+def show_time():
+    current_time = time.strftime('%H:%M:%S')
+    Label(root, text=current_time, bg="#F5E216").place(x=120, y=50)
 
-        
-        
+Button(root, text="click", command=loop, width=15, height=2, bg="#FFA62F", activebackground="#F5E216", activeforeground="#FFA500", cursor="hand1").place(x=120, y=80)
+Button(root, text="reset", command=reset, width=15, height=2, bg="#FF6347", activebackground="#F5E216", activeforeground="#FFA500", cursor="hand1").place(x=120, y=130)
+Button(root, text="Show Time", command=show_time, width=15, height=2, bg="#32CD32", activebackground="#F5E216", activeforeground="#FFA500", cursor="hand1").place(x=120, y=180)
 
-         
-        
-
-#n=tk.StringVar()
-#monthchoosen = ttk.Combobox(root, width = 15,cursor="hand1")
-# Adding combobox drop down list 
-#monthchoosen['values'] = ('sobhan allh',
-#                          'alhamdw llah',
-#                           'la alah ela allh',
-#                            'astgfer allh' )      
-
-Button(root,
-       text="click",
-       command=loop,
-       width=15,
-       height=10,
-       bg="#FFA62F",
-       activebackground="#F5E216",
-       activeforeground="#FFA500",
-       cursor="hand1").place(x=120,y=30)
-
-#monthchoosen.grid(column = 9, row = 9) 
-#monthchoosen.current() 
 root.mainloop()
-#
